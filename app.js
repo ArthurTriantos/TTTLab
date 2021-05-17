c96c43a59b72d3d42c68e0cec8ebbc7993c7da84
Arthur <arttriantos@gmail.com>
2021-05-17 12:55:01 -0500

    Getting close to wrapping up

---
+++ app.js
@@ -0,0 +1,69 @@
+//HTML Elements
+const statusDiv = document.querySelector('.status');
+const resetDiv = document.querySelector('.reset');
+const cellDivs = document.querySelector('.game-cell');
+
+//game variables
+let gameIsLive = true;
+let xIsNext = true;
+let winner = null;
+
+//functions
+const letterToSymbol = (letter) => letter === 'x' ? xSymbol : oSymbol;
+
+
+const checkGameStatus = () => {
+    const topLeft = cellDivs[0].classList[2];
+    const topMiddle = cellDivs[1].classList[2];
+    const topRight = cellDivs[2].classList[2];
+    const middleLeft = cellDivs[3].classList[2];
+    const middleMiddle = cellDivs[4].classList[2];
+    const middleRight = cellDivs[5].classList[2];
+    const bottomLeft = cellDivs[6].classList[2];
+    const bottomMiddle = cellDivs[7].classList[2];
+    const bottomRight = cellDivs[8].classList[2];
+
+    //check winner
+    if (topLeft && topLeft === topMiddle && topLeft === topRight) {
+        gameIsLive = false;
+        winner = topLeft;
+        statusDiv.innerHTML =  `${topLeft} has won!`;
+
+    }
+}
+
+
+
+
+//even Handlers
+const handleReset = (e) => {
+    console.log(e);
+};
+
+const handleCellClick = (e) => {
+    const classList = e.target.classList;
+    const location = classList[1];
+    
+    if (classList[2] === 'x' || classList[2] === 'o') {
+        return;
+    }
+
+    if (xIsNext) {
+        classList.add('x');
+        checkGameStatus();
+
+        xIsNext = !xIsNext;
+    } else {
+        classList.add('o');
+        checkGameStatus();
+        xIsNext = !xIsNext;
+    }
+};
+
+
+//event listeners
+resetDiv.addEventListener('click', handleReset);
+
+for (const cellDiv of cellDivs) {
+    cellDiv.addEventListener('click', handleCellClick)
+}
