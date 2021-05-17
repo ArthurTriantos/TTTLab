eeeda9a76345aed73762995068736f0563a87477
Arthur <arttriantos@gmail.com>
2021-05-17 13:08:51 -0500

    fixed some java

--- app.js
+++ app.js
@@ -1,33 +1,50 @@
 //HTML Elements
 const statusDiv = document.querySelector('.status');
 const resetDiv = document.querySelector('.reset');
-const cellDivs = document.querySelector('.game-cell');
+const cellDivs = document.querySelectorAll('.game-cell');
+
+// game constants
+const xSymbol = '×';
+const oSymbol = '○';
+
 
 //game variables
 let gameIsLive = true;
 let xIsNext = true;
-let winner = null;
+
 
 //functions
 const letterToSymbol = (letter) => letter === 'x' ? xSymbol : oSymbol;
 
+const handleWin = (letter) => {
+    gameIsLive = false;
+    if (letter === 'x') {
+      statusDiv.innerHTML = `${letterToSymbol(letter)} has won!`;
+    } else {
+      statusDiv.innerHTML = `<span>${letterToSymbol(letter)} has won!</span>`;
+    }
+  };
 
-const checkGameStatus = () => {
-    const topLeft = cellDivs[0].classList[2];
-    const topMiddle = cellDivs[1].classList[2];
-    const topRight = cellDivs[2].classList[2];
-    const middleLeft = cellDivs[3].classList[2];
-    const middleMiddle = cellDivs[4].classList[2];
-    const middleRight = cellDivs[5].classList[2];
-    const bottomLeft = cellDivs[6].classList[2];
-    const bottomMiddle = cellDivs[7].classList[2];
-    const bottomRight = cellDivs[8].classList[2];
+  const checkGameStatus = () => {
+    const topLeft = cellDivs[0].classList[1];
+    const topMiddle = cellDivs[1].classList[1];
+    const topRight = cellDivs[2].classList[1];
+    const middleLeft = cellDivs[3].classList[1];
+    const middleMiddle = cellDivs[4].classList[1];
+    const middleRight = cellDivs[5].classList[1];
+    const bottomLeft = cellDivs[6].classList[1];
+    const bottomMiddle = cellDivs[7].classList[1];
+    const bottomRight = cellDivs[8].classList[1];
 
     //check winner
     if (topLeft && topLeft === topMiddle && topLeft === topRight) {
         gameIsLive = false;
         winner = topLeft;
-        statusDiv.innerHTML =  `${topLeft} has won!`;
+        if (topLeft === 'x') {
+            statusDiv.innerHTML =  `${letterToSymbol(topLeft)} has won!`;
+        } else {
+            statusDiv.innerHTML = `<span>${letterToSymbol(topLeft)} has won!</span>`;
+        }
 
     }
 }
